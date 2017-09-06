1faec7fe4428292ec16e04632bdcb23450712eec
unknown <cmiu007@gmail.com>
2017-09-06 09:24:29 +0300

    continuare adresa component

--- src/app/membri/contact/adresa/adresa.component.ts
+++ src/app/membri/contact/adresa/adresa.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-adresa',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adresa.component.css']
 })
 export class AdresaComponent implements OnInit {
+  @Input ('formAdresaData')
+  formAdresaData;
 
   constructor() { }
 
