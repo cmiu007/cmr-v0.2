6c104e6f8526d92d5a07a55c24b5fc9cd77ae4d1
unknown <cmiu007@gmail.com>
2017-10-09 10:07:56 +0300

    bug validare cnp

--- src/app/services/form-validators.service.ts
+++ src/app/services/form-validators.service.ts
@@ -18,6 +18,9 @@ export class FormValidatorsService {
       for (let i = 0; i < testValCNP.length; i++) {
         sum = sum + splitCNP[i] * testValCNP[i];
       }
+      if ( sum % 11 === 10) {
+        sum = 1;
+      }
       if ((sum % 11) !== +splitCNP[12]) {
         return { 'cnpCtrlSumInvalid': true };
       } else {
